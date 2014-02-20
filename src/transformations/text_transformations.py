ac50d8e9916f7864d7103d3d16e77a76482a1bf0
Skabed <skab93@gmail.com>
2014-02-20 19:34:40 +0100

    Moved the basic Cochlea API to a transformer object (following the sklearn API, kind of). The same happened to the text transformation -> Now done by TextToNumberStructuredTransformer.

--- src/transformations/text_transformations.py
+++ src/transformations/text_transformations.py
@@ -2,44 +2,72 @@ from sklearn.feature_extraction.text import CountVectorizer
 from src.translation.data_type_translation import translate_data_type
 
 
-def transform_text_to_numbers_structured(np_array, config):
-    """
-    @param np_array: STRUCTURED Numpy array to be transformed.
-    @param config: Loaded configuration as an ordered dictionary.
-    @return: Numpy array with all the columns specified as text parsed to numbers.
-    """
-    column_indices = __find_text_column_indices(config)
-    for column_index in column_indices:
-        column_name = np_array.dtype.names[column_index]
-        column = np_array[column_name]
+class TextToNumberStructuredTransformer(object):
+    def __init__(self, config):
+        """
+        @param config: Loaded configuration as an ordered dictionary.
+        """
+        self.__config = config
+        self.__vectorizers = None
+        self.__column_indices = None
 
-        numbers = __transform_text_to_numbers(column)
+    def fit_transform(self, data):
+        """
+        @param data: STRUCTURED Numpy array to be transformed.
+        @return: Numpy array with all the columns specified as text parsed to numbers.
+        """
+        self.__vectorizers = []
+        self.__column_indices = self.__find_text_column_indices(self.__config)
+        for column_index in self.__column_indices:
+            column_name = data.dtype.names[column_index]
+            column = data[column_name]
 
-        # Create a new description so we can change the old dtype to the new numeric type.
-        old_type = np_array.dtype
-        description = old_type.descr
-        description[column_index] = (description[column_index][0], translate_data_type("float"))
+            numbers, vectorizer = self.__transform_text_to_numbers(column)
+            self.__vectorizers.append(vectorizer)
 
-        np_array[column_name] = numbers
-        np_array = np_array.astype(description)
+            # Create a new description so we can change the old dtype to the new numeric type.
+            old_type = data.dtype
+            description = old_type.descr
+            description[column_index] = (description[column_index][0], translate_data_type("float"))
 
-    return np_array
+            data[column_name] = numbers
+            data = data.astype(description)
 
+        return data
 
-def __find_text_column_indices(config):
-    column_indices = []
-    for i, (key, value) in enumerate(config["data_model"].items()):
-        data_type = value[0]
-        if __is_text_type(data_type):
-            column_indices.append(i)
-    return column_indices
+    def transform(self, data):
+        for i, column_index in enumerate(self.__column_indices):
+            column_name = data.dtype.names[column_index]
+            column = data[column_name]
 
+            numbers = self.__apply_vectorizer(column, self.__vectorizers[i])
 
-def __is_text_type(data_type):
-    return translate_data_type(data_type) == translate_data_type("string")
+            # Create a new description so we can change the old dtype to the new numeric type.
+            old_type = data.dtype
+            description = old_type.descr
+            description[column_index] = (description[column_index][0], translate_data_type("float"))
 
+            data[column_name] = numbers
+            data = data.astype(description)
 
-def __transform_text_to_numbers(text_array):
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(text_array).toarray()
-    return X.argmax(1)
+        return data
+
+    def __find_text_column_indices(self, config):
+        column_indices = []
+        for i, (key, value) in enumerate(config["data_model"].items()):
+            data_type = value[0]
+            if self.__is_text_type(data_type):
+                column_indices.append(i)
+        return column_indices
+
+    def __is_text_type(self, data_type):
+        return translate_data_type(data_type) == translate_data_type("string")
+
+    def __transform_text_to_numbers(self, text_array):
+        vectorizer = CountVectorizer()
+        x = vectorizer.fit_transform(text_array).toarray()
+        return x.argmax(1), vectorizer
+
+    def __apply_vectorizer(self, column, vectorizer):
+        x = vectorizer.transform(column).toarray()
+        return x.argmax(1)
