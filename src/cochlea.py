a498dec888f5c40cda5c8e8850e42c07d52ab9e7
Skabed <skab93@gmail.com>
2014-02-20 19:46:40 +0100

    Added one hot encoding.

--- src/cochlea.py
+++ src/cochlea.py
@@ -3,6 +3,7 @@ import collections
 
 from src.file_parsing.csv_parsing import parse_csv_structured
 from src.transformations.array_transformations import structured_array_to_ndarray
+from src.transformations.one_hot_encoding import OneHotEncodingTransformer
 from src.transformations.text_transformations import TextToNumberStructuredTransformer
 
 
@@ -10,13 +11,16 @@ class Cochlea(object):
     def __init__(self, config_file):
         self.__config = json.load(config_file, object_pairs_hook=collections.OrderedDict)
         self.__text_to_number_structured_transformer = None
+        self.__one_hot_encoding_transformer = None
 
     def fit_transform(self, data_file):
         self.__text_to_number_structured_transformer = TextToNumberStructuredTransformer(self.__config)
+        self.__one_hot_encoding_transformer = OneHotEncodingTransformer(self.__config)
 
         data = parse_csv_structured(data_file, self.__config)
         data = self.__text_to_number_structured_transformer.fit_transform(data)
         data = structured_array_to_ndarray(data)
+        data = self.__one_hot_encoding_transformer.fit_transform(data)
 
         return data
 
@@ -24,6 +28,7 @@ class Cochlea(object):
         data = parse_csv_structured(data_file, self.__config)
         data = self.__text_to_number_structured_transformer.transform(data)
         data = structured_array_to_ndarray(data)
+        data = self.__one_hot_encoding_transformer.transform(data)
 
         return data
 
---
+++ src/transformations/one_hot_encoding.py
@@ -0,0 +1,31 @@
+from sklearn.preprocessing import OneHotEncoder
+from src.translation.data_type_translation import translate_data_type
+
+
+class OneHotEncodingTransformer(object):
+    def __init__(self, config):
+        self.__config = config
+        self.__encoder = None
+
+    def fit_transform(self, data):
+        categorical_features = self.__find_categorical_features()
+        self.__encoder = OneHotEncoder(categorical_features=categorical_features, dtype=translate_data_type("float"))
+        self.__encoder.fit(data)
+        return self.__encoder.transform(data).toarray()
+
+    def transform(self, data):
+        return self.__encoder.transform(data).toarray()
+
+    def __find_categorical_features(self):
+        """
+        @return: list containing the indices of the categorical features.
+        """
+        categorical_features = []
+        for i, (key, value) in enumerate(self.__config["data_model"].items()):
+            if "categorical" in map(lambda string: string.lower(), value):
+                categorical_features.append(i)
+        return categorical_features
+
+
+
+
