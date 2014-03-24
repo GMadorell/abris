2180b9b9035aab230ba6f93ae3a8a416810309a2
Gerard Madorell <skab93@gmail.com>
2014-03-24 17:58:01 +0100

    Added basic nan treatment (mean and drop_rows only).

--- abris_transform/transformations/cleaner.py
+++ abris_transform/transformations/cleaner.py
@@ -7,6 +7,7 @@ class Cleaner(object):
 
     def prepare(self, dataframe):
         dataframe = self.__drop_ignored_features(dataframe)
+        dataframe = self.__apply_nan_treatment(dataframe)
         return dataframe
 
     def apply(self, dataframe):
@@ -18,4 +19,16 @@ class Cleaner(object):
 
     def __extract_feature_names(self, features):
         return map(lambda feature: feature.get_name(), features)
+
+    def __apply_nan_treatment(self, dataframe):
+        if self.__config.is_option_enabled("nan_treatment"):
+            method = self.__config.get_option_parameter("nan_treatment", "method")
+            if method == "mean":
+                dataframe = dataframe.fillna(dataframe.mean())
+            elif method == "drop_rows":
+                dataframe = dataframe.dropna(axis=0)
+            else:
+                raise ValueError("Method not understood: %s" % method)
+        return dataframe
+
 
