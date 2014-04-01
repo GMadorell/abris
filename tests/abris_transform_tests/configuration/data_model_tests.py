6d4d1cd2b81c9e46c9b962286f4410f61a3ef3f5
Gerard Madorell <skab93@gmail.com>
2014-04-01 17:40:17 +0200

    Changed the folder structure of the tests. Added parameter_parsing_tests.

---
+++ tests/abris_transform_tests/configuration/data_model_tests.py
@@ -0,0 +1,92 @@
+from unittest import TestCase
+
+from tests.data_model_wrapper import DataModelWrapper
+
+
+class DataModelTest(TestCase):
+    def setUp(self):
+        self.dm_wrapper = DataModelWrapper()
+
+
+    def test_has_target_without_target(self):
+        self.assertFalse(self.dm_wrapper.data_model.has_target())
+
+    def test_has_target_with_target(self):
+        self.dm_wrapper.add_numerical_target()
+        self.assertTrue(self.dm_wrapper.data_model.has_target())
+
+    def test_find_target_feature_with_one_target(self):
+        self.dm_wrapper.add_numerical_target(number=1)
+        feature = self.dm_wrapper.data_model.find_target_feature()
+        self.assertEqual(feature.get_name(), "numerical_target1")
+
+    def test_find_target_feature_without_target_should_raise(self):
+        with self.assertRaises(AssertionError):
+            self.dm_wrapper.data_model.find_target_feature()
+
+    def test_find_target_feature_with_two_targets_should_raise(self):
+        self.dm_wrapper.add_numerical_target(number=1)
+        self.dm_wrapper.add_numerical_target(number=2)
+        with self.assertRaises(AssertionError):
+            self.dm_wrapper.data_model.find_target_feature()
+
+    def test_find_all_features_empty(self):
+        self.assertEqual([], self.dm_wrapper.data_model.find_all_features())
+
+    def test_find_all_features(self):
+        self.dm_wrapper.add_numerical_feature()
+        self.dm_wrapper.add_categorical_text_feature()
+        features = self.dm_wrapper.data_model.find_all_features()
+        names = map(lambda feature: feature.get_name(), features)
+        self.assertIn("numerical_feature1", names)
+        self.assertIn("categorical_text_feature1", names)
+
+    def test_has_any_text_feature_without_text_features(self):
+        self.assertFalse(self.dm_wrapper.data_model.has_any_text_feature())
+
+    def test_has_any_text_feature_with_text_features(self):
+        self.dm_wrapper.add_categorical_text_feature()
+        self.assertFalse(self.dm_wrapper.data_model.has_any_text_feature())
+
+    def test_find_boolean_features_without_any(self):
+        self.assertEqual([], self.dm_wrapper.data_model.find_boolean_features())
+
+    def test_find_boolean_features(self):
+        self.dm_wrapper.add_boolean_feature()
+        self.dm_wrapper.add_categorical_text_feature()
+        self.dm_wrapper.add_numerical_target()
+        self.assert_feature_names(self.dm_wrapper.data_model.find_boolean_features(), "boolean_feature1")
+
+    def test_find_text_features_without_any(self):
+        self.assertEqual([], self.dm_wrapper.data_model.find_text_features())
+
+    def test_find_text_features(self):
+        self.dm_wrapper.add_boolean_feature()
+        self.dm_wrapper.add_categorical_text_feature()
+        self.dm_wrapper.add_numerical_target()
+        self.assert_feature_names(self.dm_wrapper.data_model.find_text_features(), "categorical_text_feature1")
+
+    def test_find_categorical_features_without_any(self):
+        self.assertEqual([], self.dm_wrapper.data_model.find_categorical_features())
+
+    def test_find_categorical_features(self):
+        self.dm_wrapper.add_boolean_feature()
+        self.dm_wrapper.add_categorical_text_feature()
+        self.dm_wrapper.add_numerical_target()
+        self.assert_feature_names(self.dm_wrapper.data_model.find_categorical_features(), "categorical_text_feature1")
+
+    def test_find_ignored_features_without_any(self):
+        self.assertEqual([], self.dm_wrapper.data_model.find_ignored_features())
+
+    def test_find_ignored_features(self):
+        self.dm_wrapper.add_boolean_feature()
+        self.dm_wrapper.add_ignored_numerical_feature()
+        self.dm_wrapper.add_numerical_target()
+        self.assert_feature_names(self.dm_wrapper.data_model.find_ignored_features(), "ignored_numerical_feature1")
+
+    def assert_feature_names(self, list_features, *names):
+        assert len(list_features) == len(names)
+        for i, feature in enumerate(list_features):
+            self.assertEqual(feature.get_name(), names[i])
+
+
---
+++ tests/data_model_wrapper.py
@@ -0,0 +1,55 @@
+from abris_transform.configuration.data_model import DataModel
+
+import pandas as pd
+
+
+class DataModelWrapper(object):
+    def __init__(self):
+        self.__model_dict = {}
+        self.__dataframe_dict = {}
+
+        self.dataframe = None
+        self.data_model = None
+
+        self.clean()
+        self.rebuild()
+
+    def clean(self):
+        self.__model_dict = {}
+        self.__dataframe_dict = {}
+        self.rebuild()
+
+    def rebuild(self):
+        self.dataframe = pd.DataFrame(self.__dataframe_dict)
+        self.data_model = DataModel(self.__model_dict)
+        self.data_model.set_features_types_from_dataframe(self.dataframe)
+
+    def add_numerical_feature(self, number=1):
+        self.__add(number, "numerical_feature", ["Numerical"], [1, 2])
+
+    def add_boolean_feature(self, number=1):
+        self.__add(number, "boolean_feature", ["Boolean"], [True, False])
+
+    def add_ignored_numerical_feature(self, number=1):
+        self.__add(number, "ignored_numerical_feature", ["Numerical", "Ignore"], [1, 2])
+
+    def add_categorical_numerical_feature(self, number=1):
+        self.__add(number, "categorical_numerical_feature", ["Categorical"], [1, 2])
+
+    def add_categorical_text_feature(self, number=1):
+        self.__add(number, "categorical_text_feature", ["Categorical"], ["Hi", "Hello"])
+
+    def add_numerical_target(self, number=1):
+        self.__add(number, "numerical_target", ["Numerical", "Target"], [1, 2])
+
+    def add_categorical_target(self, number=1):
+        self.__add(number, "categorical_target", ["Categorical", "Target"], [1, 2])
+
+    def __add(self, number, name, caracteristics, values):
+        self.__model_dict.update({
+            name + str(number): caracteristics
+        })
+        self.__dataframe_dict.update({
+            name + str(number): pd.Series(values)
+        })
+        self.rebuild()
